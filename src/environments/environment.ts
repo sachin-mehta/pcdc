789f68d4565ade8597df03037bd8687015a8b0d1
Victor Lopez <viclopez@unicef.com>
2025-08-04 13:46:17 +0200

    bump version to 2.0.2 in environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   // restAPI: 'https://uni-connect-services.azurewebsites.net/api/v1/',
   restAPI: env.mode === 'dev' ? env.restAPIDev : env.restAPI,
   token: env.mode === 'dev' ? env.tokenDev : token,
-  app_version: '2.0.1',
+  app_version: '2.0.2',
   appName: 'Giga Meter',
   appNameSuffix: '',
   showAboutMenu: true,
