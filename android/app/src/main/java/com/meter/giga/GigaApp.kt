bb5ad9653b6855a3416c58f5eca95fb38b333668
Nilesh Manker <nilesh.manker@nagarro.com>
2025-09-19 17:43:46 +0530

    Handled Sentry Crash Issue

--- android/app/src/main/java/com/meter/giga/GigaApp.kt
+++ android/app/src/main/java/com/meter/giga/GigaApp.kt
@@ -2,6 +2,7 @@ package com.meter.giga
 
 import android.app.Application
 import io.sentry.Sentry
+import io.sentry.android.core.SentryAndroid
 
 class GigaApp : Application() {
 
@@ -11,14 +12,12 @@ class GigaApp : Application() {
   }
 
   private fun initSentry() {
-    Sentry.init { options ->
-      options.dsn =
-        "https://e52e97fc558344bc80a218fc22a9a6a9@excubo.unicef.io/47" // Replace with your actual DSN
-      // Enable for debug builds
-      options.isDebug = BuildConfig.DEBUG
 
-      // Set Environment
-      options.environment = if (BuildConfig.DEBUG) "development" else "production"
+    SentryAndroid.init(this) { options ->
+      options.dsn = getString(R.string.sentry_dsn)
+      options.isDebug = BuildConfig.DEBUG
+      options.environment = "development" // if (BuildConfig.DEBUG) "development" else "production"
+      options.setTracesSampleRate(1.0) // capture 100% for demo
     }
   }
 }
